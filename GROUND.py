70f05c3576185827c22a4abb045c3b27ab1dbb07
Joe Brands <notsoaveragejoebrands@gmail.com>
2017-11-30 13:12:36 -0600

    Created ground, vehicle and suspension files Updated GUI

---
+++ GROUND.py
@@ -0,0 +1,7 @@
+#------------------------------------------------------------#
+# All functions having to do with the ground go in this file #
+#------------------------------------------------------------#
+
+def getdata(): # interpret the ground .txt
+
+def plotground(): # plot the ground data from the getdata
