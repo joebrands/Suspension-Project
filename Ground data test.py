b75e0ff0416885fe0460dc41890bc39182319115
mtaylor2014 <morgan.taylor12@okstate.edu>
2017-11-29 23:08:07 -0600

    test

---
+++ Ground data test.py
@@ -0,0 +1,29 @@
+import numpy as np
+import scipy.interpolate as interpolate
+import matplotlib.pyplot as plt
+
+
+# x = np.array([1, 2, 4, 5])  # sort data points by increasing x value
+# y = np.array([2, 1, 4, 3])
+
+x, y = np.loadtxt('Ground_data_test.txt', skiprows = 1, unpack = True)
+
+arr = np.arange(np.amin(x), np.amax(x), 0.01)
+s = interpolate.CubicSpline(x, y)
+
+fig, ax = plt.subplots(1, 1)
+# ax.hold(True)
+ax.plot(x, y, 'bo', label='Data Point')
+ax.plot(arr, s(arr), 'k-', label='Cubic Spline', lw=1)
+
+for i in range(x.shape[0] - 1):
+    segment_x = np.linspace(x[i], x[i + 1], 100)
+    # A (4, 100) array, where the rows contain (x-x[i])**3, (x-x[i])**2 etc.
+    exp_x = (segment_x - x[i])[None, :] ** np.arange(4)[::-1, None]
+    # Sum over the rows of exp_x weighted by coefficients in the ith column of s.c
+    segment_y = s.c[:, i].dot(exp_x)
+    ax.plot(segment_x, segment_y, label='Segment {}'.format(i), ls='--', lw=3)
+    print(segment_x)
+
+ax.legend()
+plt.show()
