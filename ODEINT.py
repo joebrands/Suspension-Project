083246cafb1d1d73d7f9b08bf851c12a4acf70fb
casey.ivanoff <casey.ivanoff@okstate.edu>
2017-12-03 23:46:55 -0600

    ODEINT

--- ODEINT.py
+++ ODEINT.py
@@ -2,77 +2,76 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
-t, y = np.loadtxt("GroundDataExample4.txt", delimiter=',', skiprows=1, unpack=True)
 
-def plotfile(t, y):
-    # file = file.tolist()
-    n = len(t)
-    print('Number of Points is', n)
-    plt.plot(t, y)
-    plt.show()
 
-def slopecalc(t1, t2, y1, y2):
-    slope = (y2 - y1) / (t2 - t1)
-    return slope
+file = np.loadtxt("GroundDataExample1.txt", delimiter=',', skiprows=1, unpack=False)
+file = file.tolist()
+h = len(file)
+u = np.zeros(h)
+r = np.zeros(h)
 
-# def pw(t1, t2, y1, y2):
-#     ymag = []
-#     ymag.append(slopecalc(t1, t2, y1, y2))
-#     # print(ymag)
-#     return ymag
+#print(file)
+for i in range(h):
+    u[i] = file[i][0]
+    r[i] = file[i][1]
+#print(u)
+#print(r)
+# poly = np.zeros(h)
+t = np.linspace(0, u[h-1], len(file))
 
-# piecewise(t, y)
+z = np.polyfit(u, r, 20)
 
-def ode(t, y):
-    # print(len(t))
-    xperm = [[0, 0, 0, 0]]
-    for i in range(len(t) - 1):
-        def ode_system(X, tODE, carparams):
-            t, y = np.loadtxt("GroundDataExample4.txt", delimiter=',', skiprows=1, unpack=True)
-            m1 = carparams[0]
-            m2 = carparams[1]
-            c1 = carparams[2]
-            k1 = carparams[3]
-            k2 = carparams[4]
 
-            print(slopecalc(t[i], t[i + 1], y[i], y[i + 1]), i, tODE)
-            y = slopecalc(t[i], t[i + 1], y[i], y[i + 1]) * tODE
+def f(z, t):
+    # return (z[0] * t ** 4) + (z[1] * t ** 3) + (z[2] * t ** 2) + (z[3] * t) + z[4]
+    # return (z[0] * t ** 6) + (z[1] * t ** 5) + (z[2] * t ** 4) + (z[3] * t ** 3) + (z[4] * t ** 2) + (z[5] * t) + z[6]
+    return (z[0] * t ** 20) + (z[1] * t ** 19) + (z[2] * t ** 18) + (z[3]* t ** 17) + (z[4] * t ** 16) + (z[5] * t ** 15) + (z[6] * t ** 14) + (z[7] * t ** 13) + (z[8] * t ** 12) + (z[9] * t**11) + (z[10]*t**10) + (z[11]*t**9) + (z[12]*t**8) + (z[13] * t**7) + (z[14] * t**6) + (z[15]*t**5) + (z[16]*t**4) + (z[17]*t**3) + (z[18] * t**2) + (z[19]*t) + z[20]
+# for i in range(len(t)):
+#     poly[i] = (f(z, t[i]))
 
-            x1 = X[0]
-            x1dot = X[1]
-            x2 = X[2]
-            x2dot = X[3]
+#
+# h = len(file)
+# r = np.zeros(h)
+# print(file)
+# for i in range(h):
+#     r[i] = file[i][1]
+# print(r)
+# x = []
+def ode_system(X, t, m1, m2,c1,k1,k2):
 
-            x1ddot = (1 / m1) * (c1 * (x2dot - x1dot) + k1 * (x2 - x1))
-            x2ddot = (1 / m2) * (-c1 * (x2dot - x1dot) - k1 * (x2 - x1) + k2 * (y - x2))
 
-            return [x1dot, x1ddot, x2dot, x2ddot]
+    # m1 = carparams[0]; m2 = carparams[1]; c1 = carparams[2]; k1 = carparams[3]; k2 = carparams[4];
 
-        tODE = t[i]
-        # tODE = np.linspace(0, len(t) - 1, len(t))
-        # print(tODE)
-        print(xperm[0][3])
-        if i == 0:
-            ic = [xperm[i][0], xperm[i][1], xperm[i][2], xperm[i][3]]
-        else:
-            ic = [xperm[i - 1][0], xperm[i - 1][1], xperm[i - 1][2], xperm[i - 1][3]]
 
-        m1 = 1
-        m2 = 1
-        c1 = 2
-        k1 = 1
-        k2 = 4
-        carparams = [m1, m2, c1, k1, k2]
-        x = odeint(ode_system, ic, tODE, args=(carparams,))
-        xperm.extend(x)
-        print(xperm)
+    y = f(z, t)
 
-    print(xperm[1][])
-    # plt.plot(t, xperm[0], 'b-', label='Body Position')
-    # #plt.plot(t, xperm[:, 1], 'g-', label='Body velocity')
-    # plt.show()
-    # # plt.plot(t, x[:, 2], 'b-', label='Wheel Position')
-    # # plt.plot(t, x[:, 3], 'g-', label='Wheel velocity')
-    # # plt.show()
 
-ode(t, y)
+    x1 = X[0]; x1dot = X[1]; x2 = X[2]; x2dot = X[3]
+
+    x1ddot = (1/m1) * (c1*(x2dot - x1dot) + k1*(x2 - x1))
+    x2ddot = (1/m2) * (-c1*(x2dot - x1dot) - k1 * (x2 - x1) + k2* (y-x2))
+
+    return [x1dot, x1ddot, x2dot, x2ddot]
+
+t = np.linspace(0, 20, 200)
+ic = [0,0,0,0]
+
+m1 = 1
+m2 = 1
+c1 = 2
+k1 = 1
+k2 = 4
+# ymag = 1
+# carparams = [m1, m2, c1, k1, k2]
+# roadparams = [ymag]
+#roadparams = r
+# print(roadparams)
+x = odeint(ode_system, ic, t, args = (m1, m2,c1,k1,k2))
+plt.title("Body")
+plt.plot(t,x[:,0], 'b-', label = 'Body Position')
+plt.plot(t,x[:,1], 'g-', label = 'Body velocity')
+plt.show()
+plt.title("Wheel")
+plt.plot(t,x[:,2], 'b-', label = 'Wheel Position')
+plt.plot(t,x[:,3], 'g-', label = 'Wheel velocity')
+plt.show()
